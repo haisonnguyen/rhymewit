f676ef846871dd125fb72982540a10e1176a351b
27kUltraHd <nhaison@pdx.edu>
2020-07-03 15:52:33 -0700

    Updates

---
+++ precache-manifest.1541ec04a0e02942b333f82cdeed63a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0a5bb7fc888b89467592cab33763d2",
+    "url": "/rhymewit/index.html"
+  },
+  {
+    "revision": "62cbde12d6e530578be8",
+    "url": "/rhymewit/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "41c68b45fe44caa6f4f3",
+    "url": "/rhymewit/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "62cbde12d6e530578be8",
+    "url": "/rhymewit/static/js/2.26e346c4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/rhymewit/static/js/2.26e346c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c68b45fe44caa6f4f3",
+    "url": "/rhymewit/static/js/main.3f4cd562.chunk.js"
+  },
+  {
+    "revision": "6ec6f392f95c902f09e9",
+    "url": "/rhymewit/static/js/runtime-main.a4c03ccf.js"
+  },
+  {
+    "revision": "a3a111dbb9f6a754513adc8ac3dbfdfe",
+    "url": "/rhymewit/static/media/girl.a3a111db.png"
+  }
+]);
